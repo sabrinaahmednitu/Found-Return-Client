26ee8bb220bada810d723dfce2b19857013e0cc8
sabrina <sabrinaahmednitu@gmail.com>
2024-10-25 02:44:08 +0600

    hero section

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,8 +1,14 @@
 
+import sea from '../../assets/sea.jpg'
+import seaD from '../../assets/dolphin.jpg'
+import { Input } from '@nextui-org/input';
 export default function Home() {
   return (
-    <section>
-    <h1>Home page</h1>
+    <section
+      className="h-[calc(100vh-64px)] bg-cover bg-center"
+      style={{ backgroundImage: `url(${sea.src})` }}
+    >
+      <Input></Input>
     </section>
   );
 }

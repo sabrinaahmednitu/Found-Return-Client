02dadd61847bb0c4f07d294b95efdc1271639ec1
sabrina <sabrinaahmednitu@gmail.com>
2024-11-11 23:17:27 +0600

    secured route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+const AuthRoutes = ['/login', '/register'];
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-
   const user = undefined;
 
-  if (user?.name) {
-    return NextResponse.next();
+  if (!user) {
+    if(AuthRoutes.includes(pathname))
   } else {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-
 }
 
 // See "Matching Paths" below to learn more

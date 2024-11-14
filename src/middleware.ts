7a6f959ab6c573852f62f8c3c38c098db97b81ca
sabrina <sabrinaahmednitu@gmail.com>
2024-11-15 00:48:34 +0600

    role based authentication start

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,22 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 const AuthRoutes = ['/login', '/register'];
+const roleBasedRoutes = {
+  USER: [/^\/profile/],
+  ADMIN: [/^\/admin/],
+};
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  console.log(pathname);
-
-  // if (!user) {
-  //   if(AuthRoutes.includes(pathname))
-  // }
+  const user = undefined;
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
 
   return NextResponse.redirect(new URL('/', request.url));
 }

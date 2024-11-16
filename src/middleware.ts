7e430d767a81b34831493b4e0bf64f8e3b24c8c4
sabrina <sabrinaahmednitu@gmail.com>
2024-11-16 22:47:55 +0600

    type declearation of role

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const AuthRoutes = ['/login', '/register'];
+const AuthRoutes = ['/login', '/register'];  
+
+type Role = keyof typeof roleBasedRoutes;
+
 const roleBasedRoutes = {
   USER: [/^\/profile/],
   ADMIN: [/^\/admin/],
@@ -10,12 +13,27 @@ const roleBasedRoutes = {
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const user = undefined;
+  const user = {
+    name: 'mir',
+    token: 'asf efd',
+    role: 'ADMIN',
+  };
+  // const user = undefined;
+  // user based authentication
   if (!user) {
     if (AuthRoutes.includes(pathname)) {
       return NextResponse.next();
     } else {
       return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+
+  //Roll based authentication
+  if (user?.role && roleBasedRoutes[user?.role as Role]) {
+    const routes = roleBasedRoutes[user?.role as Role];
+
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
     }
   }
 

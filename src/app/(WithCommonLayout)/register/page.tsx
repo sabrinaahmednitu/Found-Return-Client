c084308b8e6b1238d2ea6f75f6ad25e70693c34d
sabrina <sabrinaahmednitu@gmail.com>
2024-11-19 02:43:42 +0600

    register design done

--- src/app/(WithCommonLayout)/register/page.tsx
+++ src/app/(WithCommonLayout)/register/page.tsx
@@ -1,7 +1,52 @@
-export default function page() {
+'use client'
+import FXForm from '@/src/components/form/FXForm';
+import FXInput from '@/src/components/form/FXInput';
+import loginValidationSchema from '@/src/schemas/login.schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@nextui-org/button';
+import Link from 'next/link';
+import { FieldValues, SubmitHandler } from 'react-hook-form';
+export default function RegisterPage() {
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+      console.log(data);
+    };
   return (
     <div>
-      <h1>this is register page</h1>
+      <div>
+        <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
+          <h3 className="my-2 text-2xl font-bold">Register with FoundX</h3>
+          <p className="mb-4">Welcome ! Let's Started</p>
+          <div className="w-[35%]">
+            <FXForm
+              onSubmit={onSubmit}
+              resolver={zodResolver(loginValidationSchema)}
+            >
+              <div className="py-3">
+                <FXInput name="name" label="Name" type="name" />
+              </div>
+              <div className="py-3">
+                <FXInput name="email" label="Email" type="email" />
+              </div>
+              <div className="py-3">
+                <FXInput name="mobile" label="Mobile Number" type="number" />
+              </div>
+              <div className="py-3">
+                <FXInput name="password" label="Password" type="password" />
+              </div>
+              <Button
+                className="my-3 w-full rounded-md bg-default-900 font-semibold text-default-200"
+                size="lg"
+                type="submit"
+              >
+                Register
+              </Button>
+            </FXForm>
+            <div className="text-center">
+              Already have an account ?<Link href={'/login'}>Login</Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

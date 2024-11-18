dcd5020c9985787e4113fb645230d2080edd3d32
sabrina <sabrinaahmednitu@gmail.com>
2024-11-18 21:54:06 +0600

    form wrapper

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -1,7 +1,8 @@
-export default function page() {
+export default function LoginPage() {
   return (
       <div>
-          <h1>this is login component</h1>
+      <h1>this is login component</h1>
+      
     </div>
   );
 }
---
+++ src/components/form/FXForm.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+
+interface formConfig {
+  defaultValues?: Record<string, any>;
+  resolver?: any;
+}
+
+interface IProps extends formConfig {
+  children: ReactNode;
+  onSubmit: SubmitHandler<any>;
+}
+
+export default function FXForm({
+  children,
+  onSubmit,
+  defaultValues,
+  resolver,
+}: IProps) {
+  const formConfig: formConfig = {};
+  if (!!defaultValues) {
+    formConfig['defaultValues'] = defaultValues;
+  }
+
+  if (!!resolver) {
+    formConfig['resolver'] = resolver;
+  }
+  // methods = {handleSubmit,register,formState}
+  const methods = useForm();
+  const SubmitHandler = methods.handleSubmit;
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={SubmitHandler(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+}

31952463c78efd44d5f85aeb063df006f084e5f6
sabrina <sabrinaahmednitu@gmail.com>
2024-11-28 14:01:38 +0600

    glassmorphism loading added

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -6,39 +6,45 @@ import { Button } from '@nextui-org/button';
 import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FieldValues, SubmitHandler } from 'react-hook-form';
+import { useUserLogin } from '@/src/hooks/auth.hook';
+import Loading from '@/src/components/UI/loading';
 export default function LoginPage() {
+  const { mutate: handleUserLogin, isPending } = useUserLogin();
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    handleUserLogin(data);
   };
   return (
-    <div>
-      <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
-        <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
-        <p className="mb-4">Welcome Back ! Let's Started</p>
-        <div className="w-[35%]">
-          <FXForm
-            onSubmit={onSubmit}
-            resolver={zodResolver(loginValidationSchema)}
-          >
-            <div className="py-3">
-              <FXInput name="email" label="Email" type="email" />
-            </div>
-            <div className="py-3">
-              <FXInput name="password" label="Password" type="password" />
-            </div>
-            <Button
-              className="my-3 w-full rounded-md bg-default-900 font-semibold text-default-200"
-              size="lg"
-              type="submit"
+    <>
+      {isPending && <Loading />}
+      <div>
+        <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
+          <h3 className="my-2 text-2xl font-bold">Login with FoundX</h3>
+          <p className="mb-4">Welcome Back ! Let's Started</p>
+          <div className="w-[35%]">
+            <FXForm
+              onSubmit={onSubmit}
+              resolver={zodResolver(loginValidationSchema)}
             >
-              Login
-            </Button>
-          </FXForm>
-          <div className="text-center">
-            Don't have account ?<Link href={'/register'}>Register</Link>
+              <div className="py-3">
+                <FXInput name="email" label="Email" type="email" />
+              </div>
+              <div className="py-3">
+                <FXInput name="password" label="Password" type="password" />
+              </div>
+              <Button
+                className="my-3 w-full rounded-md bg-default-900 font-semibold text-default-200"
+                size="lg"
+                type="submit"
+              >
+                Login
+              </Button>
+            </FXForm>
+            <div className="text-center">
+              Don't have account ?<Link href={'/register'}>Register</Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

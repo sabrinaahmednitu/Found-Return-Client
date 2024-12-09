dbdea59af5043010e2e7087aeada78a9aa0b01c9
sabrina <sabrinaahmednitu@gmail.com>
2024-12-09 22:36:09 +0600

    creating form context using FormProvider

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+import FXInput from '@/src/components/form/FXInput';
+import { useFieldArray, useForm, FormProvider } from 'react-hook-form';
+
 export default function page() {
+  const methods = useForm();
+  const { control } = methods;
+  const {} = useFieldArray({
+    control,
+    name: 'questions',
+  });
   return (
-      <div>
-          <h1>create post of user</h1>
+    <div>
+      {/* making form context using FormProvider */}
+      <FormProvider {...methods}>
+        <FXInput name="title" label="Title"></FXInput>
+     </FormProvider>
     </div>
   );
 }

2e7ac5291b3dd99d4c0d16e7e5d31d8f787c6fbb
sabrina <sabrinaahmednitu@gmail.com>
2024-12-09 22:43:17 +0600

    Setting up the form for the create post page

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,20 +1,30 @@
 'use client';
 import FXInput from '@/src/components/form/FXInput';
+import { Button } from '@nextui-org/button';
 import { useFieldArray, useForm, FormProvider } from 'react-hook-form';
 
 export default function page() {
   const methods = useForm();
-  const { control } = methods;
+  const { control, handleSubmit } = methods;
   const {} = useFieldArray({
     control,
     name: 'questions',
   });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       {/* making form context using FormProvider */}
       <FormProvider {...methods}>
-        <FXInput name="title" label="Title"></FXInput>
-     </FormProvider>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FXInput name="title" label="Title"></FXInput>
+          <Button type="submit" className="mt-3">
+            Post
+          </Button>
+        </form>
+      </FormProvider>
     </div>
   );
 }

43ad5e19ec0ad8f1556ce72476759a70c8be0f72
sabrina <sabrinaahmednitu@gmail.com>
2024-12-09 20:52:11 +0600

    profile sidebar making

--- src/app/(WithCommonLayout)/(user)/profile/layout.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/layout.tsx
@@ -1,10 +1,15 @@
+import Container from '@/src/components/UI/Container';
 import { ReactNode } from 'react';
 
 export default function layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <h1>this is user layout</h1>
-      {children}
-    </>
+    <Container>
+      <div className='my-3 flex w-full gap-12'>
+        <div className="w-2/5">
+        <Sidebar />
+        </div>
+        <div className='w-4/5'>{children}</div>
+      </div>
+    </Container>
   );
 }
---
+++ src/components/UI/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+  return (
+      <div>
+          <div className="rounded-xl bg-default-100 p-2">
+              <div className="rounded-md"></div>
+          </div>
+    </div>
+  );
+}

dc13bb4c2c62ff3d90662922df3a162782316256
sabrina <sabrinaahmednitu@gmail.com>
2024-12-14 23:21:14 +0600

    Owner verifications question design

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import FXInput from '@/src/components/form/FXInput';
 import { Button } from '@nextui-org/button';
+import { Divider } from '@nextui-org/react';
 import { useFieldArray, useForm, FormProvider } from 'react-hook-form';
+import { SubmitHandler, FieldValues } from 'react-hook-form';
 
 export default function page() {
   const methods = useForm();
   const { control, handleSubmit } = methods;
-  const {} = useFieldArray({
+  const {append,remove} = useFieldArray({
     control,
     name: 'questions',
   });
@@ -20,6 +22,11 @@ export default function page() {
       <FormProvider {...methods}>
         <form onSubmit={handleSubmit(onSubmit)}>
           <FXInput name="title" label="Title"></FXInput>
+          <Divider className='my-5' />
+          <div className='flex justify-between items-center'>
+            <h1 className='text-xl'>Owner verifications question</h1>
+          <Button>Append</Button>
+          </div>
           <Button type="submit" className="mt-3">
             Post
           </Button>
